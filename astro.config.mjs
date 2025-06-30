9a937cebaa526fa52acdd2a1940cbaaa4c6f86e5
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-06-29 21:16:57 -0500

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://j4vierb.github.io',
+  base: '/portfolio/',
   integrations: [preact()]
 });
